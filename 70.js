ff1681df4c2dd7136f1082096a419809b106bf35
unknown <lbelliski@gmail.com>
2020-02-05 22:43:40 -0300

    ejercicios incompletos

--- 70.js
+++ 70.js
@@ -108,39 +108,116 @@
 
 //EJERCICIO 7
 
-const costos = [ 12.5, 56, 98, 45.75 ];
+// const costos = [ 12.5, 56, 98, 45.75 ];
 
-const agregarIVA = function (costo) {
-  return costo * 1.21;
-}
+// const agregarIVA = function (costo) {
+//   return costo * 1.21;
+// }
 
-const sumarGanancia = function (costo) {
-  return costo * 1.5;
-}
+// const sumarGanancia = function (costo) {
+//   return costo * 1.5;
+// }
 
-// codear acá la solución del ejercicio
-
-
-console.log(preciosFinales);
-// deberia mostrar
-// [ 22.6875, 101.64, 177.87, 83.03625 ]
-
-//EJERCICIO 8
-
-const productos = [ 'celular', 'notebook', 'monitor' ];
-const costos = [ 12.5, 56, 98 ];
-
-const agregarIVA = function (costo) {
-  return costo * 1.21;
-}
-
-const sumarGanancia = function (costo) {
-  return costo * 1.5;
-}
-
-// codear acá la solución del ejercicio
+// // codear acá la solución del ejercicio
 
 
-console.log(preciosFinales);
-// deberia mostrar
-// [ "celular 22.6875", "notebook 101.64", "monitor 177.87" ]
+// console.log(preciosFinales);
+// // deberia mostrar
+// // [ 22.6875, 101.64, 177.87, 83.03625 ]
+
+// //EJERCICIO 8
+
+// const productos = [ 'celular', 'notebook', 'monitor' ];
+// const costos = [ 12.5, 56, 98 ];
+
+// const agregarIVA = function (costo) {
+//   return costo * 1.21;
+// }
+
+// const sumarGanancia = function (costo) {
+//   return costo * 1.5;
+// }
+
+// // codear acá la solución del ejercicio
+
+
+// console.log(preciosFinales);
+// // deberia mostrar
+// // [ "celular 22.6875", "notebook 101.64", "monitor 177.87" ]
+
+
+// const numbers = [6, 1, 34, 94, 3, 17];
+
+// const suma = numbers.reduce((acumuladora, numero) =>{
+//   return acumuladora + numero
+// })
+// console.log(suma)
+// // deberia mostrar 155
+
+// const numbers = [6, 1, 34, 94, 3, 17];
+// const producto = numbers.reduce((acc, curr)=>{
+// return acc * curr
+// })
+// console.log(producto)
+// // codea debajo de este comentario la solucion al ejercicio
+
+// // deberia mostrar 977976
+// const notasDeTPs = [4, 7, 8, 5, 10];
+// const promedio = notasDeTPs.reduce((acc, curr) =>{
+// return acc + curr
+// },0)
+// console.log(promedio/ 5)
+// // codea debajo de este comentario la solucion al ejercicio
+
+// deberia mostrar 6.8
+const personas = [
+  {nombre: "Grace", 
+  edad: 6
+  }, 
+  {nombre: "Ada", 
+  edad: 19
+  },
+  {nombre: "Hedy", 
+  edad: 34
+  }
+];
+
+const sumaDeEdades = personas.reduce((acc,val) =>{
+return acc + val.edad;
+}, 0)
+console.log(sumaDeEdades)
+// // codea debajo de este comentario la solucion al ejercicio
+
+// // deberia mostrar 59
+// const datos = [2, -4, 6, 0, 5, -1];
+// const positivos = datos.filter((numero) => numero > 0)
+// const valorDuplicado = positivos.forEach(numero => {
+//   numero*2
+// });
+// console.log(valorDuplicado)
+// console.log(positivos)
+// // codear acá la solución del ejercicio
+// const total = 
+// const dobles = numeros.map((numero) => numero * 2)
+
+
+
+//Nil
+// const total = datos.reduce((acc,curr)=> {
+//   if(curr >= 0){
+//      return acc + curr* 2  
+//   }
+//   else{
+//       return acc
+//   }
+// }, 0)
+// console.log(total)
+//Noe
+// const datos = [2, -4, 6, 0, 5, -1];
+
+// const total = datos.filter(el => el > 0).reduce((acumuladora, valorActual) => {
+//     return acumuladora + valorActual * 2;
+// }, 0)
+
+// console.log(total);
+
